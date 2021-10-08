095473bc098b69c2c4be4347b723a5c342934b58
Sourav <rudrasisodiya786@gmail.com>
2021-10-08 14:01:32 +0530

    Initial commit

---
+++ src/webparts/azurePipelineWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAzurePipelineWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AzurePipelineWebpartWebPartStrings' {
+  const strings: IAzurePipelineWebpartWebPartStrings;
+  export = strings;
+}
